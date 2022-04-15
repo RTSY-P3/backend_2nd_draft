e4c93c8b4997c66be75db0ee64e1c0987d6fc770
tiffpereira <tiffanympereira@gmail.com>
2022-04-15 12:14:28 -0400

    added models

---
+++ models/project.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Project extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Project.init({
+    title: DataTypes.STRING,
+    body: DataTypes.TEXT,
+    image: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Project',
+  });
+  return Project;
+};
