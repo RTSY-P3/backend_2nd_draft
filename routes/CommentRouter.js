14196c43c3276cbeac878c6ecbcef59b59c0e404
tiffpereira <tiffanympereira@gmail.com>
2022-04-15 12:23:36 -0400

    transfered over routes files

---
+++ routes/CommentRouter.js
@@ -0,0 +1,25 @@
+const router = require('express').Router()
+const controller = require('../controllers/CommentController')
+const middleware = require('../middleware')
+
+router.get('/', controller.GetComments)
+router.post(
+  '/',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CreateComment
+)
+router.put(
+  '/:comment_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.UpdateComment
+)
+router.delete(
+  '/:comment_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.DeleteComment
+)
+
+module.exports = router
